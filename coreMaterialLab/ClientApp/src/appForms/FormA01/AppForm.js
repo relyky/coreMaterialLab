844366c06f9f49d0ff508ab808566329b90f4185
Rely Ky <relyky@hotmail.com>
2019-11-02 11:23:22 +0800

    add react-hooks

--- coreMaterialLab/ClientApp/src/appForms/FormA01/AppForm.js
+++ coreMaterialLab/ClientApp/src/appForms/FormA01/AppForm.js
@@ -1,17 +1,21 @@
-﻿import React, { Component } from 'react';
+﻿import React, { Fragment, useState, useEffect } from 'react';
 
 const APP_TITLE = 'FormA01'
+const APP_DESCRIPTION = 'React-Hooks test, useState, useEffect'
 
-export default class AppForm extends Component {
-    constructor(props) {
-        super(props)
-    }
+export default function AppForm() {
+    const [count, setCount] = useState(87)
 
-    render() {
-        return (
-            <div>
-                <h1>{APP_TITLE}</h1>
-            </div>
-        )
-    }
+    useEffect(() => {
+        setCount(13)
+    }, [])
+
+    return (
+        <Fragment>
+            {console.log('count', count)}
+            <h1>{APP_TITLE}</h1>
+            <p>{APP_DESCRIPTION}</p>
+            <button onClick={() => setCount(count + 1)}>{count}</button>
+        </Fragment>
+    )
 }
