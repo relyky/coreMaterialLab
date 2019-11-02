aeb15e8d366aba9e9958dc8544478dff8f9330e7
Rely Ky <relyky@hotmail.com>
2019-11-02 14:15:44 +0800

    add Material-UI

--- coreMaterialLab/ClientApp/src/appForms/FormA01/AppForm.js
+++ coreMaterialLab/ClientApp/src/appForms/FormA01/AppForm.js
@@ -1,21 +1,55 @@
-﻿import React, { Fragment, useState, useEffect } from 'react';
+﻿import React, { Fragment, useState, useEffect } from 'react'
+import Button from '@material-ui/core/Button'
+import { makeStyles } from '@material-ui/core/styles'
+import Paper from '@material-ui/core/Paper'
+import Typography from '@material-ui/core/Typography'
 
 const APP_TITLE = 'FormA01'
 const APP_DESCRIPTION = 'React-Hooks test, useState, useEffect'
 
+const useStyles = makeStyles(theme => ({
+    root: {
+        padding: theme.spacing(3, 2),
+    },
+}));
+
 export default function AppForm() {
+    const classes = useStyles();
     const [count, setCount] = useState(87)
+    const [traceList, setTraceList] = useState([])
 
     useEffect(() => {
         setCount(13)
     }, [])
+
+    function handleClick() {
+        // trace
+        traceList.push(count)
+        setTraceList(traceList)
+
+        // count
+        setCount(count + 1)
+    }
 
     return (
         <Fragment>
             {console.log('count', count)}
             <h1>{APP_TITLE}</h1>
             <p>{APP_DESCRIPTION}</p>
-            <button onClick={() => setCount(count + 1)}>{count}</button>
+            <Button variant="contained" color="primary" onClick={handleClick}>
+                {count}
+            </Button>
+            <hr />
+            <Paper className={classes.root}>
+                <Typography variant="h5" component="h3">
+                    Tracing
+                </Typography>
+                {traceList.map((c, i) =>
+                    <Typography key={i} component="p">
+                        {c}
+                    </Typography>
+                )}
+            </Paper>
         </Fragment>
     )
 }
