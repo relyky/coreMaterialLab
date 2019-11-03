9f9b9d9112e66b67fbcc0fce43345cb7769059cc
Rely Ky <relyky@hotmail.com>
2019-11-03 10:49:14 +0800

    adjust

--- coreMaterialLab/ClientApp/src/App.js
+++ coreMaterialLab/ClientApp/src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react'
 import { Route } from 'react-router'
 import Layout from './components/Layout'
-import { Home } from './components/Home'
-import { FetchData } from './components/FetchData'
-import { Counter } from './components/Counter'
+import { Home } from './appForms/Home'
+import { FetchData } from './appForms/FetchData'
+import { Counter } from './appForms/Counter'
 import FormA01 from './appForms/FormA01/AppForm'
 
 export default class App extends Component {
