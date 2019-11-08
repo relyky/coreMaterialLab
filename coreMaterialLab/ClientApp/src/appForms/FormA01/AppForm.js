2e9c4e08cb6d95c794f1ce14a4c49173a2888a23
Rely Ky <relyky@hotmail.com>
2019-11-08 22:55:41 +0800

    Timer怪奇現象

--- coreMaterialLab/ClientApp/src/appForms/FormA01/AppForm.js
+++ coreMaterialLab/ClientApp/src/appForms/FormA01/AppForm.js
@@ -1,67 +1,62 @@
 ﻿import React, { Fragment, useState, useEffect } from 'react'
 import Button from '@material-ui/core/Button'
-import { makeStyles } from '@material-ui/core/styles'
-import Paper from '@material-ui/core/Paper'
-import Typography from '@material-ui/core/Typography'
 
-const APP_TITLE = 'FormA01'
+const APP_TITLE = 'FormA01 倒數計時器與“怪奇現象”'
 const APP_DESCRIPTION = 'React-Hooks test, useState, useEffect'
 
-const useStyles = makeStyles(theme => ({
-    root: {
-        padding: theme.spacing(3, 2),
-    },
-}));
-
 export default function AppForm() {
-    const classes = useStyles();
     const [count, setCount] = useState(87)
-    const [traceList, setTraceList] = useState([])
 
     useEffect(() => {
-        setCount(13)
-    }, [])
+        console.log('useEffect', count)
+        handleReset()
+    }, []) // 等同 componentDidMount
 
-    function handleClick() {
-        // trace
-        traceList.push(count)
-        setTraceList(traceList)
+    useEffect(() => {
+        console.log('useEffect 設定計時器', count)
+        const timer = setInterval(handleInterval, 1000)
 
-        // count
+        // 此return將於unmount時觸發，可用於釋放資源，如：timer。
+        return () => clearInterval(timer) 
+    }, []); // 等同 componentDidMount
+
+    function handleInterval() {
+        console.log('interval', count) // 這個 count
+        setCount(count => count - 1)  // 與這個 count 不是同一個 count
+    }
+
+    function handleDecrease() {
+        console.log('--', count)
+        setCount(count - 1)
+    }
+
+    function handleIncrease() {
+        console.log('++', count)
         setCount(count + 1)
     }
 
     function handleReset() {
         // reset
-        setCount(0)
+        setCount(29)
     }
 
     return (
         <Fragment>
-            {console.log('count', count)}
+            {console.log('render', count)}
             <h1>{APP_TITLE}</h1>
             <p>{APP_DESCRIPTION}</p>
+            <p>請案【F12】開啟console查看Log中奇怪的現象。</p>
 
-            <Button variant="contained" color="primary" onClick={handleClick}>
-                {count}
-            </Button>
+            <h4>{count}</h4>
+            <Button variant="contained" color="primary" onClick={handleIncrease}>
+                Increase
+            </Button>&nbsp;
+            <Button variant="contained" color="primary" onClick={handleDecrease}>
+                Decrease
+            </Button>&nbsp;
             <Button variant="contained" color="secondary" onClick={handleReset}>
                 reset
             </Button>
-
-            <hr />
-
-            {/*Paper*/}
-            <Paper className={classes.root}>
-                <Typography variant="h5" component="h3">
-                    Tracing
-                </Typography>
-                {traceList.map((c, i) =>
-                    <Typography key={i} component="p">
-                        {c}
-                    </Typography>
-                )}
-            </Paper>
         </Fragment>
     )
 }
