dc68338c8a573e19e892026edbbd54bee28f11e5
Rely Ky <relyky@hotmail.com>
2019-11-25 01:15:05 +0800

    init todoMvc

--- coreMaterialLab/ClientApp/src/appForms/FormE05/AppForm.js
+++ coreMaterialLab/ClientApp/src/appForms/FormE05/AppForm.js
@@ -1,13 +1,12 @@
-﻿import React, { Fragment } from 'react'
+﻿import React, { Fragment, useState } from 'react'
 import FormTitle from '../../Layouts/FormTitle'
 import TodoList from './TodoList'
 
 export default function AppForm() {
-
-
     return (
         <Fragment>
             <FormTitle title={'這是FormE05的抬頭'} />
+            {/*<AddTodo />*/}
             <TodoList />
         </Fragment>
     )
--- coreMaterialLab/ClientApp/src/appForms/FormE05/TodoList.js
+++ coreMaterialLab/ClientApp/src/appForms/FormE05/TodoList.js
@@ -1,12 +1,15 @@
-﻿import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import Divider from '@material-ui/core/Divider';
-import InboxIcon from '@material-ui/icons/Inbox';
-import DraftsIcon from '@material-ui/icons/Drafts';
+﻿import React, { useState, useEffect } from 'react'
+import useAppInfo from '../../Hooks/useAppInfo'
+import useFormData from '../../Hooks/useFormdData'
+import t from 'typy'
+import { makeStyles } from '@material-ui/core/styles'
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemIcon from '@material-ui/core/ListItemIcon'
+import ListItemText from '@material-ui/core/ListItemText'
+import Divider from '@material-ui/core/Divider'
+import InboxIcon from '@material-ui/icons/Inbox'
+import DraftsIcon from '@material-ui/icons/Drafts'
 
 const useStyles = makeStyles(theme => ({
     root: {
@@ -14,40 +17,35 @@ const useStyles = makeStyles(theme => ({
         maxWidth: 360,
         backgroundColor: theme.palette.background.paper,
     },
-}));
-
-function ListItemLink(props) {
-    return <ListItem button component="a" {...props} />;
-}
+}))
 
 export default function SimpleList() {
-    const classes = useStyles();
+    const [appInfo, meta, setMeta] = useAppInfo()
+    const [formData, { assignValue, assignProps }] = useFormData()
+    const classes = useStyles()
+
+    // 等同建構式(constructor)
+    useState(() => {
+        assignProps({
+            itemList: [
+                { desc: '早上七點吃早餐' },
+                { desc: '中午十二點吃午餐' },
+                { desc: '下午三點喝奶茶' },
+                { desc: '晚上六點吃晚餐' },
+                { desc: '晚上十點吃宵夜' },
+            ]
+        })
+    })
 
     return (
         <div className={classes.root}>
             <List component="nav" aria-label="main mailbox folders">
-                <ListItem button>
-                    <ListItemIcon>
-                        <InboxIcon />
-                    </ListItemIcon>
-                    <ListItemText primary="Inbox" />
-                </ListItem>
-                <ListItem button>
-                    <ListItemIcon>
-                        <DraftsIcon />
-                    </ListItemIcon>
-                    <ListItemText primary="Drafts" />
-                </ListItem>
-            </List>
-            <Divider />
-            <List component="nav" aria-label="secondary mailbox folders">
-                <ListItem button>
-                    <ListItemText primary="Trash" />
-                </ListItem>
-                <ListItemLink href="#simple-list">
-                    <ListItemText primary="Spam" />
-                </ListItemLink>
+                {t(formData.itemList).isArray && formData.itemList.map((item, index) =>
+                    <ListItem key={index}>
+                        <ListItemText primary={item.desc} />
+                    </ListItem>
+                )}
             </List>
         </div>
-    );
+    )
 }
