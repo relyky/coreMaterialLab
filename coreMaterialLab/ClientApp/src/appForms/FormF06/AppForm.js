559e92a6ce5da95604318e3a802686cfdb719915
Rely Ky <relyky@hotmail.com>
2019-12-11 13:47:03 +0800

    step

--- coreMaterialLab/ClientApp/src/appForms/FormF06/AppForm.js
+++ coreMaterialLab/ClientApp/src/appForms/FormF06/AppForm.js
@@ -1,18 +1,22 @@
-﻿import React, { Fragment, useEffect } from 'react'
-import useAppInfo from '../../Hooks/useAppInfo'
-import useFormData from '../../Hooks/useFormdData'
+﻿import React, { Fragment } from 'react'
+//import useAppInfo from '../../Hooks/useAppInfo'
+//import useFormData from '../../Hooks/useFormdData'
+import WithMemo from './WithMemo'
 
 const APP_TITLE = 'FormF06'
-const APP_DESCRIPTION = 'useMemo & useCallback'
+const APP_DESCRIPTION = 'useMemo 可以避免在每次 render 都進行昂貴的計算。This optimization helps to avoid expensive calculations on every render.'
 
 export default function AppForm() {
-    const [appInfo, meta, setMeta] = useAppInfo()
-    const [formData, { assignValue, assignProps }] = useFormData()
+    //const [appInfo, meta, setMeta] = useAppInfo()
+    //const [formData, { assignValue, assignProps }] = useFormData()
 
     return (
         <Fragment>
             <h1>{APP_TITLE}</h1>
             <p>{APP_DESCRIPTION}</p> 
+            <p>請案【F12】開啟console查看Log中，是否有進行expensive calculations。</p>
+
+            <WithMemo />
         </Fragment>
     )
 }
---
+++ coreMaterialLab/ClientApp/src/appForms/FormF06/WithMemo.js
@@ -0,0 +1,32 @@
+﻿import React, { useState, useMemo } from 'react'
+
+export default function WithMemo() {
+    const [count, setCount] = useState(1)
+    const [val, setValue] = useState('')
+
+    function expensiveCalculate(count,flag) {
+        console.log('compute', { count, flag })
+        let sum = 0
+        for (let i = 0; i < count * 100; i++) {
+            sum += i
+        }
+        return sum;
+    }
+
+    const expensive = expensiveCalculate(count,'')
+    const expensiveMemo = useMemo(() => expensiveCalculate(count, 'M'), [count])
+
+    console.log('render')
+    return (
+        <div style={{ borderStyle: 'solid', borderWidth:1, padding:'0.5em' }}>
+
+            <h4>{count} - {expensive} - {expensiveMemo}</h4>
+            <span>{val}</span>
+
+            <div>
+                <button onClick={() => setCount(count + 1)}>Count</button>
+                <input value={val} onChange={event => setValue(event.target.value)} placeholder='val' />
+            </div>
+        </div>
+    )
+}
